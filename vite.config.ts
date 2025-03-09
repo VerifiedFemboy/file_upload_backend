5c9ed58ea7798c4bbf5a4edf87f2ba4a33fc73ef
iadyo <iamadyoo@gmail.com>
2025-03-09 01:31:17 +0100

    kurwa

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'https://verifiedfemboy.github.io/maciekfancyuploader/',
+  base: 'https://verifiedfemboy.github.io/file_upload_backend/',
 })
